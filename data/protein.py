fixing masking and cropping

data/protein.py
@@ -163,16 +163,16 @@ class TorchProtein:
         field_data.mask_data(mask)
 
     def random_crop_mask(self, crop_len=400):
-      start = random.randint(0, len(self.data) - crop_len)
+      start = random.randint(0, len(self.aatype.data) - crop_len)
       end = start + crop_len
-      mask = torch.zeros(len(self.data))
+      mask = torch.zeros(len(self.aatype.data))
       mask[start:end] = 1
       return mask.bool()
 
     def center_crop_mask(self, crop_len=400):
-        start = len(self.data) // 2 - crop_len // 2
+        start = len(self.aatype.data) // 2 - crop_len // 2
         end = start + crop_len
-        mask = torch.zeros(len(self.data))
+        mask = torch.zeros(len(self.aatype.data))
         mask[start:end] = 1
         return mask.bool()
 