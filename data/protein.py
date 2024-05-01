be3345b4059a7da77e283b4f2fb055d62f62c051
Dylan Abramson <dabramson@wesleyan.edu>
2024-04-30 21:31:03 -0400

    debugging

--- data/protein.py
+++ data/protein.py
@@ -162,6 +162,7 @@ class TorchProtein:
         if field in ignore_mask_fields:
             continue
         field_data = getattr(self, field)
+        print(foeld_data.type_)
         field_data.mask_data(mask)
 
     def random_crop_mask(self, crop_len=400):
