74a47a5af538182008388150732971bed669c41e
Dylan Abramson <dabramson@wesleyan.edu>
2024-05-02 15:01:17 -0400

    add transform from atom14 to atom37

--- data/openfold/transforms.py
+++ data/openfold/transforms.py
@@ -512,6 +512,19 @@ def atom37_to_frames(protein, eps=1e-8):
     return protein
 
 
+def atom14_to_atom37(atom14, batch):
+    atom37_data = batched_gather(
+        atom14,
+        batch["residx_atom37_to_atom14"],
+        dim=-2,
+        no_batch_dims=len(atom14.shape[:-2]),
+    )
+
+    atom37_data = atom37_data * batch["atom37_atom_exists"][..., None]
+
+    return atom37_data
+
+
 def get_chi_atom_indices():
     """Returns atom indices needed to compute chi angles for all residue types.
 
@@ -568,7 +581,7 @@ def atom37_to_torsion_angles(
         "(prefix)torsion_angles_mask" ([*, N_res, 7])
             Torsion angles mask
     """
-    
+
     device = "cpu" if protein[prefix + "aatype"].device.type == "cpu" else "cuda"
     with disable_tf32(), torch.autocast(device_type=device, enabled=False):
         aatype = protein[prefix + "aatype"]
