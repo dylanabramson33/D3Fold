c2eeb0d8c73ae873e4d8a1df7099386b38c2b3f3
Dylan Abramson <dabramson@wesleyan.edu>
2024-05-03 13:44:11 -0400

    im an idiot

--- data/openfold/transforms.py
+++ data/openfold/transforms.py
@@ -16,7 +16,6 @@
 import numpy as np
 import torch
 from contextlib import contextmanager
-from torch_geometric.nn import radius_graph
 
 from D3Fold.data.openfold import residue_constants as rc
 from D3Fold.data.openfold.rigid_utils import Rotation, Rigid
@@ -27,7 +26,7 @@ from D3Fold.data.openfold.vector import Vec3Array
 from D3Fold.data.openfold.tensor_utils import (
     batched_gather,
 )
-
+from torch import cdist
 
 @contextmanager
 def disable_tf32():
@@ -742,30 +741,16 @@ def get_chi_angles(protein):
 
     return protein
 
-def get_distance_matrix(protein, r=10, pad_size=400):
+def get_distance_mat_stack(protein, min_radius=5, max_radius=26, num_radii=8):
     CA_INDEX = rc.atom_types.index("CA")
     ca_pos = protein["all_atom_positions"][:, CA_INDEX]
-    ca_mask = protein["all_atom_mask"][:, CA_INDEX]
 
-    nan_mask = torch.where(ca_mask == 0, torch.nan, torch.ones_like(ca_mask))
-    nan_mask = nan_mask.unsqueeze(-1)
-    ca_pos = ca_pos * nan_mask
-    ca_pos = ca_pos.view(-1, 3)
-    contact_edges = radius_graph(ca_pos, r=r)
-    num_residues = max(ca_pos.shape[0], pad_size)
-    contact_mat = torch.zeros((num_residues, num_residues))
-    src_indices, tgt_indices = contact_edges[0], contact_edges[1]
-    contact_mat[src_indices, tgt_indices] = 1
-    contact_mat[tgt_indices, src_indices] = 1
-    return contact_mat
-
-def get_distance_mat_stack(protein, min_radius=5, max_radius=26, num_radii=8):
-    radius_list = np.linspace(min_radius, max_radius, num_radii)
-    stack = []
-    for r in radius_list:
-        mat = get_distance_matrix(protein, r=r)
-        stack.append(mat)
-
-    stack = torch.stack(stack, dim=-1)
-    protein["distance_mat_stack"] = stack
+    dists = cdist(ca_pos, ca_pos)
+    bins = torch.linspace(2, 22, 64)
+    buckets = torch.bucketize(dists, bins)
+    distograms = torch.zeros(ca_pos.shape[0], ca_pos.shape[0], 65)
+    padded = torch.zeros(400, 400, 65)
+    scattered = distograms.clone().scatter_(2, buckets.unsqueeze(-1), True)
+    padded[:dists.shape[0], :dists.shape[0], :] = scattered
+    protein["distance_mat_stack"] = padded
     return protein
