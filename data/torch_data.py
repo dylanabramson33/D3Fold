8f5bc0c9112f8d6091434ba704e37ac09b32fe36
Dylan Abramson <dabramson@wesleyan.edu>
2024-05-10 20:52:31 -0400

    temp fix

--- data/torch_data.py
+++ data/torch_data.py
@@ -149,14 +149,16 @@ class Collator:
             del batch_data[f"{self.follow_key}_ptr"]
 
         for key in seq_data_list[0].keys():
-            if not self.type_dict[key].pair_type or key == "mask":
+            is_meta_data = self.type_dict[key].get("meta_data", False)
+            if is_meta_data:
+                batch_data[key] = torch.tensor([d[key] for d in seq_data_list])
+            # this is bad 
+            if key == "mask" or not self.type_dict[key].pair_type:
                 list_of_tensors = [d[key] for d in seq_data_list]
                 seq = pad_seqrep(list_of_tensors, key)
             elif self.type_dict[key].pair_type:
                 list_of_tensors = [d[key] for d in seq_data_list]
                 seq = pad_pairrep(list_of_tensors)
-            elif self.type_dict[key].meta_data:
-                batch_data[key] = torch.tensor([d[key] for d in seq_data_list])
         batch_data[key] = seq
         _, _, batch_tokens = batch_converter(raw_seq_data_list)
         batch_data.tokens = batch_tokens
