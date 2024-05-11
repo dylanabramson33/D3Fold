0b7e4971b69f25bad01d3d1131280f6534d8efef
Dylan Abramson <dabramson@wesleyan.edu>
2024-05-10 21:02:17 -0400

    oops

--- data/torch_data.py
+++ data/torch_data.py
@@ -155,14 +155,15 @@ class Collator:
 
         for key in seq_data_list[0].keys():
             if self.type_dict[key].meta_data:
-                batch_data[key] = torch.tensor([d[key] for d in seq_data_list])
+                seq = torch.tensor([d[key] for d in seq_data_list])
             elif not self.type_dict[key].pair_type:
                 list_of_tensors = [d[key] for d in seq_data_list]
                 seq = pad_seqrep(list_of_tensors, key)
             elif self.type_dict[key].pair_type:
                 list_of_tensors = [d[key] for d in seq_data_list]
                 seq = pad_pairrep(list_of_tensors)
-        batch_data[key] = seq
+                
+            batch_data[key] = seq
         _, _, batch_tokens = batch_converter(raw_seq_data_list)
         batch_data.tokens = batch_tokens
 
