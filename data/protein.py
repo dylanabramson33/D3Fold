744f785bb8988252706b6d9109f5e9f08c0ea3be
Dylan Abramson <dabramson@wesleyan.edu>
2024-05-23 10:19:14 -0400

    switch transform order

--- data/protein.py
+++ data/protein.py
@@ -218,6 +218,6 @@ class TorchProtein:
       tensor_dic = transforms.get_backbone_frames(tensor_dic)
       tensor_dic = transforms.get_chi_angles(tensor_dic)
       tensor_dic = transforms.get_distance_mat_stack(tensor_dic)
-      tensor_dic = transforms.get_quantized_phi_psi_omega(tensor_dic)
       tensor_dic = transforms.convert_angles_to_degrees(tensor_dic)
+      tensor_dic = transforms.get_quantized_phi_psi_omega(tensor_dic)
       return cls.from_dict(tensor_dic, type_dict)
